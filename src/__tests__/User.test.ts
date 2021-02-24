c68b3b879eb39612ef724d37ae3697c0ff28331f
angeloplacebo <public.angelo@gmail.com>
2021-02-24 17:22:12 -0300

    day 3

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,11 +1,31 @@
 import request from 'supertest'
 import { app } from '../app'
 
-describe("Users", () => {
+import createConnection from '../database'
 
-  request(app).get("/users")
-  .send({
-    email: "user@example.com",
-    name:"User Example"
+describe("Users", async () => {
+  beforeAll(async () => {
+    const connection = await createConnection()
+    await connection.runMigrations()
+  })
+
+  it("should be able to create a new user", async () => {
+
+    const response = await request(app).post("/users")
+      .send({
+        email: "user@example.com",
+        name: "User Example"
+      })
+    expect(response.status).toBe(201)
+  })
+
+  it("should not be able to create a user exists email", async () => {
+    const response = await request(app).post("/users")
+      .send({
+        email: "user@example.com",
+        name: "User Example"
+      })
+    expect(response.status).toBe(400)
   })
 })
+
